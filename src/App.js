d7bde84cd328cb5dd8f54d084666727a71e18132
Bitajune <beheshtibita@gmail.com>
2020-02-24 13:44:10 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Todo from "./components/Todo";
+import Chat from "./components/Chat";
+
 import "./App.css";
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <Home />
+      <Login />
+      <Signup />
+      <Todo />
+      <Chat />
+    </div>
+  );
 }
 
 export default App;
