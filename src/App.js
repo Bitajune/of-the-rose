7eae2eeb8518219b58488263ca805001d116028b
Bitajune <beheshtibita@gmail.com>
2020-02-25 09:19:24 -0800

    error boundary

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from "react";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Todo from "./components/Todo";
-import Chat from "./components/Chat";
 import Map from "./components/Map";
 import MeetUp from "./components/MeetUp";
 import Reminder from "./components/Reminder";
+import Chat from "./components/Chat";
+import Todo from "./components/Todo";
 
 import "./App.css";
 
